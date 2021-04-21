043740bf0427a32022bbabb038276c00cf2b5119
Landry Agbaholou <landry.agbaholou@gmail.com>
2021-04-21 22:13:17 +0200

    Initialize project

---
+++ src/test/java/it/carbon/exercice/treasuremap/TreasureMapApplicationTests.java
@@ -0,0 +1,13 @@
+package it.carbon.exercice.treasuremap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreasureMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
